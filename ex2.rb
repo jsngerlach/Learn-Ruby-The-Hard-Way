98396dce560ba3ec2e4eb666ebc0d49bfcc49a2b
Jason Gerlach <jsngerlach@yahoo.com>
2017-02-25 10:37:21 -0800

    first commit

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+#t Acomment, this so you can read your program later.
+#Anything after the # is ignored by ruby
+
+puts "I could have code like this." #and the comment after is ignored
+
+# you can also commennt to "disable" or comment out a piece of code:
+#puts "this won't run."
+
+puts "This will run."
---
+++ ex5.rb
@@ -0,0 +1,15 @@
+my_name = 'Jason  E. Gerlach'
+my_age = 25 #year
+my_height = 71 #inches
+my_weight = 250 # pounds of muscle steel and sex appeal
+my_eyes = 'Green' #color, amount is 2
+my_teeth = 'White' # color,amount is 32
+my_hair = 'Blonde' # color, amount is unkown
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's weighs #{my_weight} pounds"
+puts "Pure muscle!"
+puts "He has #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are #{my_teeth}."
+puts "Add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
